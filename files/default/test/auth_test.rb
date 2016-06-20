884feef156fbac04bb1796801ca1788b044e6aa9
Gennadiy Nerubayev <gennadiy.nerubayev@schrodinger.com>
2016-06-20 18:04:14 -0400

    A bit of a hack to keep retrying anonymous bind

--- files/default/test/auth_test.rb
+++ files/default/test/auth_test.rb
@@ -1,6 +1,16 @@
 describe_recipe 'openldap::auth' do
   it 'can bind anonymously' do
     output = %x(/usr/bin/ldapwhoami -x 2>&1)
+
+    # Server may take a bit of time to respond sometimes.
+    retries = 10
+    until ( /anonymous/ =~ output ) || retries == 0 do
+      printf "sleeping for a bit because retries is #{retries}"
+      sleep 1
+      retries -= 1
+    end
+
+    # Last chance at success
     assert_match /anonymous/, output
     assert_equal 0, $?.exitstatus
   end
