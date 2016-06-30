53f13a0b511ea8badea559fedb1cebfa4151137a
Gennadiy Nerubayev <gennadiy.nerubayev@schrodinger.com>
2016-06-30 14:18:37 -0400

    Fix logic a bit as variable is only assigned once

--- files/default/test/auth_test.rb
+++ files/default/test/auth_test.rb
@@ -1,17 +1,15 @@
 describe_recipe 'openldap::auth' do
   it 'can bind anonymously' do
-    output = %x(/usr/bin/ldapwhoami -x 2>&1)
 
     # Server may take a bit of time to respond sometimes.
     retries = 10
-    until ( /anonymous/ =~ output ) || retries == 0 do
-      printf "sleeping for a bit because retries is #{retries}"
+    until ( /anonymous/ =~ %x(/usr/bin/ldapwhoami -x 2>&1) ) || retries == 0 do
       sleep 1
       retries -= 1
     end
 
     # Last chance at success
-    assert_match /anonymous/, output
+    assert_match /anonymous/, %x(/usr/bin/ldapwhoami -x 2>&1)
     assert_equal 0, $?.exitstatus
   end
 end
